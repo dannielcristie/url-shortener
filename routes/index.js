9b05da1ff7701e321659355c7ee9354119310351
Danniel Cristie <dany14012@gmail.com>
2021-12-10 23:48:09 -0300

    init

---
+++ routes/index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+const Link = require('../models/link');
+var router = express.Router();
+
+router.get('/:code/stats', async (req, res, next) =>{
+  const code = req.params.code;
+  const results = await Link.findOne({where:{code} });
+  if(!results) return res.sendStatus(404);
+  res.render('stats',results.dataValues);
+})
+
+router.get('/:code', async (req, res, next) =>{
+  const code = req.params.code;
+
+  const results = await Link.findOne({where:{code} });
+  if(!results) return res.sendStatus(404);
+
+  results.hits++;
+  await results.save();
+
+  res.redirect(results.url);
+});
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Encurtador' });
+});
+
+function generateCode() {
+  let text = '';
+  const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+};
+
+
+router.post('/new', async(req, res, next) => {
+  const url = req.body.url;
+  const code = generateCode();
+
+  const result = await Link.create({
+    url,
+    code
+  }); 
+
+  res.render('stats',result.dataValues);
+
+});
+
+module.exports = router;
